a1a5c43af07ebbea832c42e1812d0df8e7bbffe4
ritika-bisht-19 <bisht.ritika19200@gmail.com>
2025-07-30 09:43:50 +0530

    [1351] [Time Beats: 100.00%] [Memory Beats: 36.24%] - LeetPush

--- LeetCode/Count-Negative-Numbers-in-a-Sorted-Matrix.cpp
+++ LeetCode/Count-Negative-Numbers-in-a-Sorted-Matrix.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
-        int m = grid.size();
-    int n = grid[0].size();
-    int count = 0;
-    int i = m - 1, j = 0;  
-
-    while (i >= 0 && j < n) {
-        if (grid[i][j] < 0) {
-            count += (n - j);  
-            i--;  
-        } else {
-            j++;  
+        int row=grid.size();
+        int col=grid[0].size();
+        int i=row-1;
+        int j=0;
+        int cnt=0;
+        while(i>=0 && j<col)
+        {
+            if(grid[i][j]<0)
+            {
+                cnt+=col-j;
+                i--;
+            }
+            else j++;
         }
-    }
-
-    return count;
+        return cnt;
     }
 };
